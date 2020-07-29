75f45139e429d1cbff667af08c25d2441054cd61
mikelbts89 <mikelbts89@gmail.com>
2020-07-29 21:11:15 +0300

    adding-product-class

--- app.ts
+++ app.ts
@@ -1,3 +1,19 @@
 class Product {
   name: string;
+  price: number;
+  weight: number;
+  kosher: boolean;
+  manufacturer: string;
+  expiryDate: number;
+  constructor(_name, _price, _weight, _kosher, _manufacturer, _expiryDate) {
+    this.name = _name;
+    this.price = _price;
+    this.weight = _weight;
+    this.kosher = _kosher;
+    this.manufacturer = _manufacturer;
+    this.expiryDate = _expiryDate;
+  }
 }
+const product1 = new Product("milk", 5.99, 1, true, "tnuva", 17.8);
+const product2 = new Product("bread", 2.69, 1, true, "Angel", 1.8);
+const product3 = new Product("butter", 12000, 1, true, "tnuva", 23.1);
--- js/app.js
+++ js/app.js
@@ -1,6 +1,15 @@
 var Product = (function () {
-    function Product() {
+    function Product(_name, _price, _weight, _kosher, _manufacturer, _expiryDate) {
+        this.name = _name;
+        this.price = _price;
+        this.weight = _weight;
+        this.kosher = _kosher;
+        this.manufacturer = _manufacturer;
+        this.expiryDate = _expiryDate;
     }
     return Product;
 }());
+var product1 = new Product("milk", 5.99, 1, true, "tnuva", 17.8);
+var product2 = new Product("bread", 2.69, 1, true, "Angel", 1.8);
+var product3 = new Product("butter", 12000, 1, true, "tnuva", 23.1);
 //# sourceMappingURL=app.js.map
